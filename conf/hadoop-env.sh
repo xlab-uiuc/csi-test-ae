c1fef8b2f2be98b1b90a831e3a160e05c8a3e3f5
Chaitanya B Bhandari <chaitanyabhushanbhandari@gmail.com>
2023-01-29 20:18:03 -0600

    Using 'dpkg --print-architecture' in JAVA_HOME to make artifact more platform independent.

--- conf/hadoop-env.sh
+++ conf/hadoop-env.sh
@@ -51,7 +51,8 @@
 
 # The java implementation to use. By default, this environment
 # variable is REQUIRED on ALL platforms except OS X!
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export arch="$(dpkg --print-architecture)"
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-$arch
 
 # Location of Hadoop.  By default, Hadoop will attempt to determine
 # this location based upon its execution path.
